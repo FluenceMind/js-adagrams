ff603b85811422776597b35a754590b34a635bf9
Tatiana Didenko <tatiana.a.didenko@gmail.com>
2025-12-02 01:40:57 +0300

    Complete Wave 3: implement scoreWord and finish incomplete test

--- src/adagrams.js
+++ src/adagrams.js
@@ -8,7 +8,17 @@ const LETTER_POOL = {
   Z: 1,
 };
 
-
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1,
+  L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4,
+  Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10,
+};
 
 export const drawLetters = () => {
   const letterPoolArray = [];
@@ -53,7 +63,22 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  if (!word) {
+    return 0;
+  }
+
+  const upperWord = word.toUpperCase();
+  let score = 0;
+
+  for (const char of upperWord) {
+    score += SCORE_CHART[char] || 0;
+  }
+
+  if (upperWord.length >= 7 && upperWord.length <= 10) {
+    score += 8;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
